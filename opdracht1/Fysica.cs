3c53ee27f02176a460e914d30c0d2f9b488f085b
Freek <freekgj@hotmail.com>
2019-11-19 23:34:17 +0100

    first commit

---
+++ opdracht1/Fysica.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Fysica : MonoBehaviour
+{
+    public Vector3 Force;
+    public Vector3 Acceleration;
+    public Vector3 Velocity;
+    public int mass;
+    public Vector3 startForce;
+    // Start is called before the first frame
+    // update
+void Start()
+    {
+        Force = new Vector3(0, -1, 0);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Acceleration = Force / mass;
+        Velocity += Acceleration *
+        Time.deltaTime;
+        transform.position += Velocity *
+        Time.deltaTime;
+    }
+}
